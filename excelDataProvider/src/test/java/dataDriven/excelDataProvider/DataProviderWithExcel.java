b359513098a4daa7d0f099b3b0d1e24bd66fb02c
sachin <patesachin9@gmail.com>
2023-05-09 13:42:18 +0530

    branch develop test1

--- excelDataProvider/src/test/java/dataDriven/excelDataProvider/DataProviderWithExcel.java
+++ excelDataProvider/src/test/java/dataDriven/excelDataProvider/DataProviderWithExcel.java
@@ -19,7 +19,8 @@ public class DataProviderWithExcel
 	public void testData(String s1,String s2,String s3)
 	{
 		System.out.println(s1 +" "+s2 +" "+ s3);
-			
+		//System.out.println(rowCount);	
+       //System.out.println(rowCount);			
 	}
 		
 	@DataProvider(name="driverTest")
